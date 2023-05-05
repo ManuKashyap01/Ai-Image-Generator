b941c69d6003a8fcdc5f5264581850b0a7430af7
ManuKashyap01 <kashyap.manu200201@gmail.com>
2023-05-05 10:05:18 +0530

    user interface complete and initialized server directory

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
 
-const FormField = () => {
+const FormField = ({label,type,name,placeholder,value,handleChange,isSurpriseMe,handleSurpriseMe}) => {
   return (
-    <div>FormField</div>
+    <div>
+      <div className="flex items-center gap-2 mt-5 mb-3">
+        <label htmlFor={name} className='block text-sm font-medium text-gray-900'>
+          {label}
+        </label>
+        {isSurpriseMe && (
+          <button
+            type='button'
+            onClick={handleSurpriseMe}
+            className='font-semibold text-xs bg-[#bada55] py-1 px-2 rounded-md'
+          >
+            Surprise me
+          </button>
+        )}
+      </div>
+      <input 
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className='bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#bada55] focus:border-[#bada55] outline-none border w-full p-3 block'
+      />
+    </div>
   )
 }
 
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,93 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { useNavigate } from 'react-router-dom'
+import { preview } from '../assets'
+import {getRandomPrompt} from '../utils'
+import { FormField,Loader } from '../components'
 
 const CreatePost = () => {
+  const navigate=useNavigate()
+  const [formdata, setformdata] = useState({
+    name:'',
+    prompt:'',
+    photo:'',
+  })
+  const [loadingimg, setloadingimg] = useState(false)
+  const [loading, setloading] = useState(false)
+
+  const handleSubmit=()=>{
+
+  }
+
+  const handleChange=(e)=>{
+    setformdata({...formdata,[e.target.name]:e.target.value})
+  }
+
+  const handleSurpriseMe=()=>{
+    const randomPrompt=getRandomPrompt(formdata.prompt)
+    setformdata({...formdata,prompt:randomPrompt})
+  }
+
+  const generateImg=()=>{
+
+  }
   return (
-    <div>CreatePost</div>
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-4xl">
+          Create Something New
+        </h1>
+        <p className='md:text-lg font-medium text-[#bada55] text-md italic'>"Let the world see your creativity"</p>
+        <p className="mt-2 text-[#666e75] text-sm max-w-[500px]">
+          Create imaginative and visually stunning images through DALL-E AI Second Draft and share them with the community
+        </p>
+      </div>
+      <form onSubmit={handleSubmit} className="mt-12 max-w-3xl">
+        <div className="flex flex-col gap-2">
+          <FormField
+            label="Your name"
+            type="text"
+            name="name"
+            placeholder="Manu kashyap"
+            value={formdata.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            label="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="A toy robot sitting on red wall waiting to fall"
+            value={formdata.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+
+          <div className="relative mt-2.5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-bada55 focus:border-bada55 w-64 p-3 h-64 flex justify-center items-center">
+            {formdata.photo?(
+              <img src={formdata.photo} alt={formdata.prompt} className='w-full h-full object-contain'/>
+            ):(
+              <img src={preview} alt="preview" className='w-9/12 h-9/12 object-contain opacity-40' />
+            )}
+           {loadingimg && (
+            <div className="absolute inset-0 z-0 flex justify-center items-center bg-black/[0.5] rounded-lg">
+              <Loader/>
+            </div>
+           )}
+          </div>
+        </div>
+        <div className="mt-5 flex gap-5">
+          <button type='button' onClick={generateImg} className='text-white bg-[#bada55] px-5 py-2.5 rounded-md w-full sm:w-auto text-center font-medium tracking-wide' >
+            {loadingimg ? "Generating..." : "Generate"}
+          </button>
+        </div>
+        <div className="mt-10">
+            <p className="mt-2 text-[#666e75] text-sm">Once you have created your image, You can even share it on the community timeline!</p>
+            <button type='submit' className='mt-3 text-white bg-[#bada55] px-5 py-2.5 rounded-md w-full sm:w-auto text-center font-medium tracking-wide' >
+            {loading ? "Sharing..." : "Share with the community"}
+          </button>
+        </div>
+      </form>
+    </section>
   )
 }
 
