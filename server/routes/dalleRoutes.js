c42507ecd2502683c9636ff3a80eca36e3f30364
ManuKashyap01 <kashyap.manu200201@gmail.com>
2023-05-14 00:16:16 +0530

    some minor changes

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import * as dotenv from 'dotenv'
 import fetch from 'node-fetch'
-import { Configuration,OpenAIApi } from "openai";
+// import { Configuration,OpenAIApi } from "openai";
 
 dotenv.config()
 
